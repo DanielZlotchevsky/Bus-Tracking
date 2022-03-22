d2c129e0f0673e3749ccac31550d436b69b01368
DanielZlotchevsky <danjerz77@gmail.com>
2022-03-22 06:31:13 -0400

    Fixed zoom to fit all Markers

---
+++ README.md
@@ -0,0 +1,10 @@
+# Boston Line 15 Bus Tracker
+
+## Description
+This program uses two seperate APIs, MapBox and MTBA, to generate a map of boston and place marker, and to get data of both buses and bus stops to plug into the map. This program tracks the location of all Route 15 buses and stops in particular.
+
+## Usage
+After loading this program onto your machine, you can either follow the API documentation found at https://docs.mapbox.com/mapbox-gl-js/guides/, or if you are looking to track vehicles and stops in the same general location, you can replace the URLs at the bottom of the JavaScript file to suit your needs.
+
+## Potential Future Updates
+A couple of improvements I will be looking to make are to add a GPS feature or to add arrival times if a specific bus stop is clicked.
---
+++ busTracking.js
@@ -0,0 +1,70 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuemxvdCIsImEiOiJjbDEwM3AzZ2YyZ3UzM2JwNGhrbmJmOXVxIn0.1LeaAgT5l-rHeu41RzyBpg';
+
+document.getElementById('timeDate').innerText = new Date();
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.05856, 42.31807],
+    zoom: 12.2
+});
+
+var allStops = []
+var allBuses = []
+
+
+//Generates Bus Stop Markers
+async function createBusStops() {
+    const busStop = await getBusStops();
+    busStop.forEach((stop, i) => {
+        allStops[i] = new mapboxgl.Marker({
+            color: '#FFA500',
+        })
+        .setLngLat([-71.03867885945147, 42.35007028512333])
+        .addTo(map);
+        allStops[i].setLngLat([busStop[i].attributes.longitude, busStop[i].attributes.latitude])
+    });
+
+}
+
+async function createBuses() {
+    const buses = await getBusLocations();
+    buses.forEach((bus, i) => {
+        allBuses[i] = new mapboxgl.Marker()
+        .setLngLat([-71.104081, 42.365554])
+        .addTo(map);
+    });
+    //setTimeout(createBuses, 10000)
+    run()
+}
+
+
+
+async function run(){
+    const buses = await getBusLocations();
+    buses.forEach((bus, i) => {
+        allBuses[i].setLngLat([buses[i].attributes.longitude, buses[i].attributes.latitude])
+    });
+    setTimeout(run, 15000)
+}
+
+
+createBusStops()
+createBuses()
+
+
+// Request bus data from MBTA
+async function getBusLocations(){
+    const url = 'https://api-v3.mbta.com/vehicles?filter[route]=15&include=trip';
+    const response = await fetch(url);
+    const json     = await response.json();
+    return json.data;
+}
+
+// Request bus data from MBTA
+async function getBusStops(){
+    const url = 'https://api-v3.mbta.com/stops?filter[route]=15';
+    const response = await fetch(url);
+    const json     = await response.json();
+    return json.data;
+}
